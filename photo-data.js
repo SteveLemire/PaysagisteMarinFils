4fb4c6e5f1c74b2048cdac818ade41d54acbb190
Steve Lemire <slemire@voxtel.com>
2022-10-10 18:44:59 -0400

    Dynamically generate the list of realizations from a json file to ensure that list of expertise and realization photo have the same title. This also make it ver easy to generate anchor to the specified photos.

---
+++ photo-data.js
@@ -0,0 +1,43 @@
+var photoData =
+[
+    {
+        "title": "Aménagement Paysager",
+        "fileName": "AmenagementPaysager",
+        "fileCount": 4
+    },
+    {
+        "title": "Muret",
+        "fileName": "Muret",
+        "fileCount": 2
+    },
+    {
+        "title": "Patio de piscine (pavé collé)",
+        "fileName": "PatioPiscinePaveColler",
+        "fileCount": 2
+    },
+    {
+        "title": "Pavé unis",
+        "fileName": "PaveUnis",
+        "fileCount": 6
+    },
+    {
+        "title": "Pierre collé",
+        "fileName": "PierreColler",
+        "fileCount": 1
+    },
+    {
+        "title": "Réparation de cheminé",
+        "fileName": "ReparationChemine",
+        "fileCount": 4
+    },
+    {
+        "title": "Réparation de pavé",
+        "fileName": "ReparationPave",
+        "fileCount": 5
+    },
+    {
+        "title": "Réparation de pierre naturelles",
+        "fileName": "ReparationPierreNaturel",
+        "fileCount": 2
+    }
+]
